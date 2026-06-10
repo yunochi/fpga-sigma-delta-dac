6d0fa640b44e3353e77077e686d1cbc2fa96f484
Yuno <yuno@yunochi.com>
2026-06-10 14:24:24 +0900

    wip

--- fw/copy_patch.sh
+++ fw/copy_patch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp xemacpsif_physpeed.c ../sigma-delta-dac/vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
-cp xemacpsif_hw.c ../sigma-delta-dac/vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
+cp xemacpsif_physpeed.c ./vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
+cp xemacpsif_hw.c ./vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
