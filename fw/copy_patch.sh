61bc739b9c0092d6bf04f7cb137c71a8beeb9a21
Yuno <yuno@yunochi.com>
2026-06-14 03:48:07 +0900

    ethernet patch 수정

--- fw/copy_patch.sh
+++ fw/copy_patch.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 cp xemacpsif_physpeed.c ./vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
-cp xemacpsif_hw.c ./vitis/platform/psu_cortexa53_0/standalone_psu_cortexa53_0/bsp/libsrc/lwip220/src/lwip-2.2.0/contrib/ports/xilinx/netif/
